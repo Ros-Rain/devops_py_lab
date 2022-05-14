83bcb1c3054918b6ad73eff03cd65606013c148b
Roman Osadchiy <ros.rain@gmail.com>
2022-05-14 11:54:06 +0300

    V1 finished

--- py/main_ver_1.py
+++ py/main_ver_1.py
@@ -1,13 +1,23 @@
-# import data_source_ver_N as ds
-import markdown2output_ver_1.py  as m2o
+import os
+import markdown2output_ver_1 as m2o
 
-tmpl_category_file="tmpl/category_ver_1.md"
-tmpl_slides_file="tmpl/slides_ver_1.md"
+home_abspath = os.path.dirname(os.path.abspath(__file__))
 
-out_md_file="out/slides_ver_1.md"
-out_pptx_file="out/slides_ver_1.md"
+tmpl_category_file = os.path.abspath(home_abspath + "/../tmpl/category_ver_1.md")
+tmpl_slides_file = os.path.abspath(home_abspath + "/../tmpl/slides_ver_1.md")
 
-pass # объединить файлы в один в директории out 
+out_md_file=os.path.abspath(home_abspath + "/../out/slides_ver_1.md")
+out_pptx_file=os.path.abspath(home_abspath + "/../out/slides_ver_1.pptx")
+
+# объединить файлы в один в директории out
+tmpl_files = []
+tmpl_files.append(tmpl_category_file)
+tmpl_files.append(tmpl_slides_file)
+
+with open(out_md_file, "w") as out_file:
+    for f in tmpl_files:
+        with open(f) as in_file:
+            out_file.write(in_file.read())
 
 m2o.convert_to_pptx(out_md_file, out_pptx_file)  # преобразовать в pptx 
 
